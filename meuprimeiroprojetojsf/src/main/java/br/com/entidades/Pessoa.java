5014f856b457996d942b9de98d284c39ca6059b1
yanva <yanva@DESKTOP-T57VRJB>
2022-06-02 22:15:52 -0300

    Projeto JSF

---
+++ meuprimeiroprojetojsf/src/main/java/br/com/entidades/Pessoa.java
@@ -0,0 +1,124 @@
+package br.com.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String sobrenome;
+	private Integer idade;
+	private String sexo;
+	private String[] frameworks;
+	private Boolean ativo;
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+
+	
+	
+	//---------------------
+	
+	public Pessoa() {
+		
+	}
+	
+	//---------------------
+	
+	public void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	public Boolean getAtivo() {
+		return ativo;
+	}
+
+	public String[] getFrameworks() {
+		return frameworks;
+	}
+	public void setFrameworks(String[] frameworks) {
+		this.frameworks = frameworks;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
